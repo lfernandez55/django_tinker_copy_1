bfe30884456540ac449116b3f1eba71b9b9726ee
lfernandez55 <lfernandez@weber.edu>
2019-01-23 20:00:42 -0700

    Extending the login form so that email is added and also displayed

--- learn_u/conference/accounts/views.py
+++ learn_u/conference/accounts/views.py
@@ -2,17 +2,33 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+# Basic form
+# def signup(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             print('debugHERE valid form')
+#             user = form.save()
+#             auth_login(request, user)
+#             return redirect('homeward')
+#         else:
+#             print('debugHERE INVALID form')
+#     else:
+#
+#         form = UserCreationForm()
+#     return render(request, 'signup.html', {'form': form})
+
+
+
+from .forms import SignUpForm
+#extending the form
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            print('debugHERE valid form')
             user = form.save()
             auth_login(request, user)
             return redirect('homeward')
-        else:
-            print('debugHERE INVALID form')
     else:
-
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
