b016ecf831a0834f10cf488635751a3a71b3b854
lfernandez55 <lfernandez@weber.edu>
2019-01-23 19:49:53 -0700

    Signup functionality now working
    Very basic signup page that redirects to home and then displays the username at the top of the form.  Notice here that there is a change to sqllite3 (presumably becuz the user was added to the db)

--- learn_u/conference/accounts/views.py
+++ learn_u/conference/accounts/views.py
@@ -1,7 +1,18 @@
-
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            print('debugHERE valid form')
+            user = form.save()
+            auth_login(request, user)
+            return redirect('homeward')
+        else:
+            print('debugHERE INVALID form')
+    else:
+
+        form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
