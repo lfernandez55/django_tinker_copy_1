f734df496ef99922568dc94e523900119f49ae33
lfernandez55 <lfernandez@weber.edu>
2019-01-30 19:54:37 -0700

    My first commit

--- learn_u/conference/conference/settings.py
+++ learn_u/conference/conference/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'u2',
-    'accounts'
+    'accounts',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lfernandez\documents\github\django_tinker_copy_1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
