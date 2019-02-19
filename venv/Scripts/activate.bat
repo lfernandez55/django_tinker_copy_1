ee1bea0e975a19b92136406cc90593043a1151c9
lfernandez55 <lfernandez@weber.edu>
2019-02-18 22:30:14 -0700

    Finally totally working!!!

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\lfernandez\Documents\GitHub\django_tinker_copy_1\venv"
+set "VIRTUAL_ENV=C:\Users\Luke_Lenovo\Documents\GitHub\django_tinker_copy_1\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
